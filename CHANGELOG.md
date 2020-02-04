1a2cde5c98d829a79e646ce15b10a8a53d802979
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2020-02-04 16:16:28 +0700

    update version and metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ cachethq CHANGELOG
 
 This file is used to list changes made in each version of the cachethq cookbook.
 
+1.1.0
+-----
+- [Fadli] - Allow setup CachetHQ with apache2 and CAS
+
 1.0.0
 -----
 - [Bimlendu Mishra] - Initial release of CachetHQ
--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'justbimlendu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures CachetHQ'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
